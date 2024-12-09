b4be04c05d712f5b5ba8d4bc7764fabbe7f54bc1
calvin-key <calvin.delanop7@gmail.com>
2024-12-09 14:22:36 +0700

    feat: keep login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useAppSelector } from "@/redux/hooks";
+
 export default function Home() {
-  return <h1>Homepage</h1>;
+  const user = useAppSelector((state) => state.user);
+  return <h1>Welcome back {user.name}</h1>;
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,22 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logoutAction } from "@/redux/slices/userSlice";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter(); // kalau mau setelah logout page berubah ke login page
+  const user = useAppSelector((state) => state.user);
+
+  const logout = () => {
+    localStorage.removeItem("blog-storage");
+    dispatch(logoutAction());
+    router.push("/login"); // kalau mau setelah logout page berubah ke login page
+  };
+
   return (
     <nav className="bg-slate-200">
       <div className="container mx-auto px-4">
@@ -9,9 +25,12 @@ const Navbar = () => {
             Logo
           </Link>
 
-          <div className="flex items-center gap-8 font-medium">
+          <div className="flex cursor-pointer items-center gap-8 font-medium">
             <Link href="/">Home</Link>
             <Link href="/">Profile</Link>
+            {!user.id && <Link href="/login">Sign In</Link>}
+            {!!user.id && <Button onClick={logout}>Logout</Button>}
+            {/* {user.id ? <p>Logout</p> : <Link href="/">Sign In</Link>} bisa juga seperti ini */}
           </div>
         </div>
       </div>
