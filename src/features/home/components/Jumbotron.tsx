18dbcbf93f981388d25c65294a63568f24c33af9
calvin-key <calvin.delanop7@gmail.com>
2024-12-10 15:47:05 +0700

    feat: blog list

---
+++ src/features/home/components/Jumbotron.tsx
@@ -0,0 +1,12 @@
+const Jumbotron = () => {
+  return (
+    <section className="mt-20 flex flex-col items-center">
+      <h1 className="text-6xl font-bold">The Bloghub</h1>
+      <p className="text-xl">
+        A blog about anything that you could ever wish for (I lied)
+      </p>
+    </section>
+  );
+};
+
+export default Jumbotron;
